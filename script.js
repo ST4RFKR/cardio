9580a794bb4e706280b5a8f47e5dddd050027a21
Your Name <you@example.com>
2024-06-05 11:46:38 +0300

    ref clas

--- script.js
+++ script.js
@@ -9,63 +9,82 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputTemp = document.querySelector('.form__input--temp');
 const inputClimb = document.querySelector('.form__input--climb');
 
-let map, mapEvent;
 
-if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(
-        function(positin)
-        {
-            const {latitude} = positin.coords;
-            const {longitude} = positin.coords;
+
+class App {
+
+  #map;
+  #mapEvent;
+    constructor() {
+      this._getPositiont();
+
+      form.addEventListener('submit', this._newWorkout.bind(this))
+      inputType.addEventListener('change', this._toggleClimbField);
+    }
+
+    _getPositiont () {
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(
+              this._loadMap.bind(this),
+              function () {
+                  alert('НЕВОЗМОЖНО ПОЛУЧИТЬ ВАШЕ МЕСТОПОЛОЖЕНИЕ')
+              })
+        }
+    }
+    _loadMap (position) {
+            const {latitude} = position.coords;
+            const {longitude} = position.coords;
             // console.log(`https://www.google.com.ua/maps/@${latitude}.${longitude}z?entry=ttu`);
 
             const coords = [latitude, longitude]
 
-            map = L.map('map').setView(coords, 12.5);
+            this.#map = L.map('map').setView(coords, 12.5);
 
 
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+            }).addTo(this.#map);
 
             L.marker(coords)
-                .addTo(map)
-                .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-                .openPopup();
+              .addTo(this.#map)
+              .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+              .openPopup();
 
-        map.on('click', function(event){
-            mapEvent = event;
-            form.classList.remove('hidden');
-            inputDistance.focus();
+      this.#map.on('click', this._showForm.bind(this));
 
-
-        });
-  
- 
-        },
-        function () {
-            alert('НЕВОЗМОЖНО ПОЛУЧИТЬ ВАШЕ МЕСТОПОЛОЖЕНИЕ')
-        })
+    }
+    _showForm (e) {
+      this.#mapEvent = e;
+      form.classList.remove('hidden');
+      inputDistance.focus();
+    }
+    _toggleClimbField () {
+      inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+      inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+    _newWorkout (e) {
+      e.preventDefault();
+      inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = '';
+      inputDistance.focus();
+      const {lat, lng} = this.#mapEvent.latlng;
+      L.marker([lat,lng])
+        .addTo(this.#map)
+        .bindPopup(L.popup({
+          maxWidth : 250,
+          maxHeight : 	100,
+          autoClose : false,
+          closeOnClick : false,
+          className : `running-popup`
+        }))
+        .setPopupContent(`Тренировка`)
+        .openPopup();
+    }
 }
-form.addEventListener('submit', function(e){
-    e.preventDefault();  
-    inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = '';
-    inputDistance.focus();
-    const {lat, lng} = mapEvent.latlng;
-            L.marker([lat,lng])
-            .addTo(map)
-            .bindPopup(L.popup({
-                maxWidth : 250,
-                maxHeight : 	100,
-                autoClose : false,
-                closeOnClick : false,
-                className : `running-popup`
-            }))
-            .setPopupContent(`Тренировка`)
-            .openPopup();
 
-        } )
-inputType.addEventListener('change', function () {
-    inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
-    inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
-})
+const app = new App();
+// app._getPositiont();
+
+
+
+
+
