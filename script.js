6562f6b23a4d107e77a9c870678ec6bf11fc2d36
Your Name <you@example.com>
2024-06-05 13:43:16 +0300

    add workout obj

--- script.js
+++ script.js
@@ -10,12 +10,48 @@ const inputTemp = document.querySelector('.form__input--temp');
 const inputClimb = document.querySelector('.form__input--climb');
 
 
+class  Workout {
+  date = new Date();
+  id = ( Date.now() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  type = 'running'
+  constructor(coords, distance, duration, temp) {
+    super(coords, distance, duration);
+    this.temp = temp;
+    this.calcPace();
+  }
+  calcPace () {
+    this.pace = this.duration / this.distance;
+  }
+}
+
+class Cycling extends Workout {
+  type = 'cycling'
+  constructor(coords, distance, duration, climb) {
+    super(coords, distance, duration);
+    this.climb = climb;
+    this.calcSpeed();
+  }
+  calcSpeed () {
+    this.speed = this.distance / this.duration / 60;
+  }
+}
 
 class App {
 
   #map;
   #mapEvent;
+  #workouts = [];
+
     constructor() {
+
       this._getPositiont();
 
       form.addEventListener('submit', this._newWorkout.bind(this))
@@ -34,7 +70,7 @@ class App {
     _loadMap (position) {
             const {latitude} = position.coords;
             const {longitude} = position.coords;
-            // console.log(`https://www.google.com.ua/maps/@${latitude}.${longitude}z?entry=ttu`);
+
 
             const coords = [latitude, longitude]
 
@@ -64,17 +100,72 @@ class App {
     }
     _newWorkout (e) {
       e.preventDefault();
-      inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = '';
-      inputDistance.focus();
       const {lat, lng} = this.#mapEvent.latlng;
-      L.marker([lat,lng])
+      let workout;
+
+      const areNumbers =  (...numbers) =>  numbers.every((numb) => Number.isInteger(numb));
+      const areNumbersPositive = (...numbers) => numbers.every((numb) => numb > 0);
+      const clearInput = () => {
+        inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = '';
+      }
+      //Получить данные из формы
+      const type = inputType.value;
+      const distance =  +inputDistance.value;
+      const duration = +inputDuration.value;
+
+      //Если тренировка является пробежкой создать обьект Running
+      if (type === 'running') {
+        const temp = +inputTemp.value;
+          //Проверить валидность даннных
+          if (
+        //        !Number.isInteger(distance)
+          //   || !Number.isInteger(duration)
+          //   || !Number.isInteger(temp)
+        !areNumbersPositive(distance,duration,temp) ||
+        !areNumbers(distance,duration,temp))
+            return clearInput(), alert('Введите число');
+      workout = new Running([lat, lng] , distance, duration, temp);
+
+      }
+      //Если тренировка является пробежкой создать обьект Cycling
+      if (type === 'cycling') {
+        //Проверить валидность даннных
+        const climb = +inputClimb.value;
+        if (
+          //        !Number.isInteger(distance)
+          //   || !Number.isInteger(duration)
+          //   || !Number.isInteger(climb)
+          !areNumbersPositive(distance,duration) ||
+          !areNumbers(distance,duration,climb))
+          return clearInput(), alert('Введите число');
+        workout = new Cycling([lat, lng] , distance, duration, climb);
+
+      }
+
+      this.#workouts.push(workout);
+      console.log(workout);
+      //Добаить новый обьект в массив тренироваок
+
+      //Отобразить треноровнку на карте
+      this.displayWorkout(workout);
+      inputDistance.focus();
+
+
+
+      //Очистить поля формы
+
+      clearInput();
+
+    }
+    displayWorkout (workout) {
+      L.marker(workout.coords)
         .addTo(this.#map)
         .bindPopup(L.popup({
           maxWidth : 250,
           maxHeight : 	100,
           autoClose : false,
           closeOnClick : false,
-          className : `running-popup`
+          className : `${workout.type}-popup`
         }))
         .setPopupContent(`Тренировка`)
         .openPopup();
@@ -82,7 +173,7 @@ class App {
 }
 
 const app = new App();
-// app._getPositiont();
+
 
 
 
